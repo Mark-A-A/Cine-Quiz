f4e0031c0043e0aab27ba9eadd5cbf2b8c5875d2
Mark-A-A <markawai01@gmail.com>
2019-05-19 16:38:24 -0400

    API helper with axios added

---
+++ client/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const API = {
+  getAllCharacters: async ()=>{
+    return axios.get("/api/characters");
+  },
+  submitAnswers: function(query) {
+    return axios.post("/api/quiz/answers", { params: { q: query } });
+  }
+}
