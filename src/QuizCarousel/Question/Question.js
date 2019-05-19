41ee9b80d5a830492af64eea8495fa6fe0165d66
Mark-A-A <markawai01@gmail.com>
2019-05-19 11:46:40 -0400

    Question answering added

--- src/QuizCarousel/Question/Question.js
+++ src/QuizCarousel/Question/Question.js
@@ -1,34 +1,42 @@
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 
 import "./question.scss"
 
 export function Question({
-  img: src,
   currentId,
+  character,
+  img: src,
   question,
   choices,
+  userAnswer,
+  handleAnswer
 }){
   const [choice1, choice2, choice3] = choices
 
+  const onChooseAnswer = (e) => {
+    const { value } =(e.target);
+    handleAnswer(currentId, value)
+  } 
+  
   return (
     <section className="question-section">
-      <poster>
-        <img src={src}/>
-      </poster>
-      <question>
+      <div className="poster">
+        <img src={src} alt={character}/>
+      </div>
+      <div className="question">
         <h4>{question}</h4>
-        <choices>
-          <choice> 
-            <input type="radio" name={currentId} value={choice1}/> {choice1}<br/>
-          </choice>
-          <choice> 
-            <input type="radio" name={currentId} value={choice2}/> {choice2}<br/>
-          </choice>
-          <choice> 
-            <input type="radio" name={currentId} value={choice3}/> {choice3}<br/>
-          </choice>
-        </choices>
-      </question>
+        <ul className="answer-choices">
+          <li className="choice">
+            <input type="radio" name={currentId} value={choice1} onChange={onChooseAnswer} checked={userAnswer === choice1 }/> {choice1}<br/>
+          </li>
+          <li className="choice">
+            <input type="radio" name={currentId} value={choice2} onChange={onChooseAnswer} checked={userAnswer === choice2} /> {choice2}<br/>
+          </li>
+          <li className="choice">
+            <input type="radio" name={currentId} value={choice3} onChange={onChooseAnswer} checked={userAnswer === choice3} /> {choice3}<br/>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 
--- src/QuizCarousel/QuizCarousel.js
+++ src/QuizCarousel/QuizCarousel.js
@@ -1,8 +1,11 @@
 
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 import { Navigation } from "./Navigation"
 import { Question } from "./Question"
+
+
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
 import characters from './marvel.json'
 
@@ -12,11 +15,29 @@ export class QuizCarousel extends Component {
     currentId: 1,
     characters: characters,
   }
+
+  componentDidMount(){
+    const reducer = (acc, char, i) => {
+      acc[i + 1] = null
+      return acc
+    }
+    const answersInitial = characters.reduce(reducer, {})
+    
+    this.setState({
+      answers: answersInitial
+    })
+  }
+
+  handleAnsweringQuestion = (id, answer) => {
+    let { answers: answersNextState } = this.state
+    answersNextState[id] = answer
+    this.setState({
+      answers: answersNextState
+    })
+  }
   
   handleMainArrowClick = (val) => {
-    const {
-      currentId,
-      characters
+    const { currentId,
     } = this.state
     
     const nextValue = currentId + val
@@ -36,26 +57,35 @@ export class QuizCarousel extends Component {
     const {
       currentId,
       characters,
-      // currentCharacter
+      answers
     } = this.state
+  
     const currentCharacter = characters[currentId - 1]
+    
+    const currentAnswer = answers ? answers[currentId] : null
 
     return (
       <div className="quiz-carousel">
         <div className="arrow-button-wrapper left">
           <button disabled={currentId === 1} onClick={() => this.handleMainArrowClick(-1)}>
-            <img className="arrow-button-left" src="./assets/arrow.png" />
+            <FaAngleLeft className="arrow-button-left"/>
           </button>
         </div>
         <div className="quiz-body">
           <Navigation currentId={currentId} characters={characters} handleClick={this.setCharacter} />
           {
-            currentCharacter && <Question {...currentCharacter} currentId={currentId} />
+            currentCharacter && 
+            <Question
+              currentId={currentId}
+              handleAnswer={this.handleAnsweringQuestion}
+              userAnswer={currentAnswer}
+              {...currentCharacter}
+            />
           }
         </div>
         <div className="arrow-button-wrapper right">
           <button disabled={currentId === characters.length} onClick={() => this.handleMainArrowClick(1)}>
-            <img className="arrow-button-right" src="./assets/arrow.png" />
+            <FaAngleRight className="arrow-button-right" />
           </button>
         </div>
       </div>
